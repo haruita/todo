098807ceb6d644c1066082c4f969627dce5e200a
akeboshi <116474348+haruita@users.noreply.github.com>
2026-03-12 21:00:26 -0300

    Create backend

---
+++ backend/src/server.ts
@@ -0,0 +1,115 @@
+import express from 'express';
+const morgan = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(express.text());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+interface Task {
+    id: number,
+    title: string,
+    description: string,
+    completed: boolean,
+    createdAt: string
+}
+
+let tasks: Task[] = [
+    {
+        id: 0,
+        title: 'Mi primera nota',
+        description: '¡Los saludo desde el backend!',
+        completed: true,
+        createdAt: Date.now().toString()
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send('Esta es una aplicación para tomar notas.');
+})
+
+app.get('/api/tasks', (req, res) => {
+    res.json(tasks);
+})
+
+app.post('/api/tasks', (req, res) => {
+    const maxId = tasks.length > 0 ? Math.max(...tasks.map(t => t.id)) : 0;
+    const newTask = ({
+        id: maxId + 1,
+        ...req.body,
+        createdAt: Date.now().toString()
+    });
+    tasks.push(newTask);
+    res.send(newTask);
+})
+
+app.get('/api/tasks/:id', (req, res) => {
+    const { id } = req.params;
+
+    if (!id) return res.sendStatus(404).json({
+        message: "ID not provided."
+    })
+
+    const auxId = parseInt(id);
+    const taskFound = tasks.find((t) => t.id === auxId);
+
+    if (!taskFound) return res.status(404).json({
+        message: "Task not found."
+    })
+
+    res.send(taskFound);
+})
+
+app.put('/api/tasks/:id', (req, res) => {
+    const newData = req.body;
+    const { id } = req.params;
+    
+
+    if (!id) return res.sendStatus(404).json({
+        message: "ID not provided."
+    })
+    
+    const auxId = parseInt(id);
+    const taskExists = tasks.some((t) => t.id === auxId);
+
+    if (!taskExists) {
+        return res.status(404).json({ message: "Task not found." });
+    }
+    
+    tasks = tasks.map((t) =>
+        t.id === auxId ? { ...t, ...newData, id: auxId } : t
+    );
+
+    const updatedTask = tasks.find(t => t.id === auxId);
+    res.json(updatedTask);
+})
+
+app.delete('/api/tasks/:id', (req, res) => {
+    const { id } = req.params;
+
+    if (!id) return res.sendStatus(404).json({
+        message: 'ID not provided.'
+    })
+
+    const auxId = parseInt(id);
+    const taskFound = tasks.find((t) => t.id === auxId);
+
+    if (!taskFound)
+        return res.status(404).json({
+            message: "Task not found."
+        })
+
+    tasks = tasks.filter((t) => t.id !== parseInt(req.params.id));
+    res.sendStatus(204);
+})
+
+app.use((req, res) => {
+    res.status(404).send('No se encontró el recurso.');
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}.`);
+});
